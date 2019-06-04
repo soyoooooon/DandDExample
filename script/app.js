78cba366efc889ad739dc33d4e19b99d45a41e89
이소윤 <YOON@isoyun-ui-MBP.fanshawec.ca>
2019-06-04 18:16:33 -0400

    Trevor so appreciate

---
+++ script/app.js
@@ -0,0 +1,68 @@
+(()=>{
+	console.log("Trevor");
+	// set up the puzzle pieces and boards
+
+	const piecesBoard = document.querySelector(".puzzle-pieces"),
+		  puzzleBoard = document.querySelector(".puzzle-board"),
+		  puzzleSelectors = document.querySelectorAll(".buttonHolder img"),
+		  dropZones = document.querySelectorAll(".drop-zone");
+
+	
+	let draggablePieces = piecesBoard.querySelectorAll("img");
+
+
+
+
+	function switchImage() {
+		console.log(this);
+	
+
+		//grab the corresponding background image ( 0, 1, 2 or 3)
+		//and get it from the images folder (backGround1.jpg as an examole)
+		let bgImage=`./images/backGround${this.dataset.puzzleref}.jpg`;
+
+		//set the background image style on the dropzone container//
+		puzzleBoard.style.backgroundImage = `url(${bgImage})`;
+	}
+
+
+
+
+
+	puzzleSelectors.forEach(thumbnail => { thumbnail.addEventListener("click", switchImage); });
+
+	//loop through the draggable images
+
+	//this lets us drag stuff => not that hard
+	draggablePieces.forEach(piece => {
+		piece.addEventListener("dragstart", function(e){
+			console.log("draggin..");
+
+	 		//dataTransfer has two methods, a setter and getter
+	 		//set data on the drag, and retrieve it on the drop
+	 		e.dataTransfer.setData("text/plain", this.id);	
+		});
+	});
+
+	//this is the dragover and drop functionality => this is for the drop zones
+	dropZones.forEach(zone =>{
+		//allow user to drag over an element
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log("dragged sumpin over me");
+		});
+
+		//allow a user to drop an element
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log("you dropped sumpin on me");
+
+
+			let draggedElement = e.dataTransfer.getData("text/plain");
+			console.log("you dragged: " , draggedElement);
+
+			e.target.appendChild(document.querySelector(`#${draggedElement}`));
+		});
+	}) 
+	
+})();
